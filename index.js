5c7bd5208f649f7a18bdbba74c6e86e2e93cd4c7
Matheus Amâncio Ferreira <matheus.ferreira3@estudante.ufla.br>
2021-03-01 14:56:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var http = require('http')
-
-http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-type": "text/plain"})
-response.end("Hello World")
-}).listen(process.env.PORT)
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
